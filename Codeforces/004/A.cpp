a6d3c7a096987d76d4bcfa95dfd22eed4092bab2
Sourav Mondal <117431788+nxtsourav7@users.noreply.github.com>
2023-06-20 20:38:25 +0600

    solved -Watermelon 4A

--- Codeforces/004/A.cpp
+++ Codeforces/004/A.cpp
@@ -1,5 +1,35 @@
+/*
+*	author 	: nxtsourav7
+*	problem : Watermelon
+*	created : Dec/04/2022 15:13
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
+
+void solve() {
+	int w;
+  cin >> w;
+
+  if(w%2 == 0 && w!=2) {
+    cout << "YES" << endl;
+  }
+  else {
+    cout << "NO" << endl;
+  }
+	
+}
 int main(void) {
-  return 0;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	/* TEST CASE */
+	int tc = 1;
+	// cin >> tc;
+	for(int t=1 ; t<=tc ; t++) {
+		// cout << "case #" << t << ": ";
+		solve();
+	}
+
+	return 0;
 }
