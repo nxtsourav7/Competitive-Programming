095b76260a161c7f837084492a218b57ef74c5a8
nxtsourav7 <nxtsourav7@gmail.com>
2023-06-21 11:10:24 +0600

    added basic templete

---
+++ Templete/basic.cpp
@@ -0,0 +1,36 @@
+/*
+*	author 	: nxtsourav7
+*	problem : 
+*	created : 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* DEFINE */
+#define ll long long
+#define ld long double
+#define read(x) int x;cin >> x
+#define all(x) (x).begin(),(x).end()
+
+/* CONSTANT */
+const ll MOD = 1e9+7;
+const ll INF = 1e9;
+
+void solve() {
+	
+}
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	/* TEST CASE */
+	int tc = 1;
+	cin >> tc;
+	for(int t=1 ; t<=tc ; t++) {
+		// cout << "case #" << t << ": ";
+		solve();
+	}
+
+	return 0;
+}
