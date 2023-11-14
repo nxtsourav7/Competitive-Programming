4d2b68161c66098d950bbe85a0988935f8229bab
nxtsourav7 <nxtsourav7@gmail.com>
2023-11-14 18:12:42 +0600

    lcm problem

---
+++ Codeforces/1389/A.cpp
@@ -0,0 +1,36 @@
+/*
+*	author 	: nxtsourav7
+*	problem : LCM Problem
+*	created : 2023-11-14 18:12:05
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void deal_with() {
+	int l , r;
+    cin >> l >> r;
+
+    if(2*l <= r) {
+        cout << l << " " << 2*l;
+    }
+    else {
+        cout << "-1 -1"; 
+    }
+}
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	/* TEST CASE */
+	int tc = 1;
+	cin >> tc;
+	for(int t=1 ; t<=tc ; t++) {
+		// cout << "case #" << t << ": ";
+		deal_with();
+        cout << "\n";
+	}
+
+	return 0;
+}
