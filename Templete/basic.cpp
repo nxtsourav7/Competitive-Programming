7c4a45b37342e101eb3d289b6d7bd23563d8af50
nxtsourav7 <nxtsourav7@gmail.com>
2023-12-27 19:01:08 +0600

    c

--- Templete/basic.cpp
+++ Templete/basic.cpp
@@ -1,27 +1,28 @@
 /**
  *	author 	: nxtsourav7
- *	problem : 
  *	created : 
 **/
 
 #include<bits/stdc++.h>
+long long int T = 1;
 using namespace std;
 
 
-#define endl "\n"
+#define nl "\n"
+#define f first 
+#define s second
 #define int long long 
 #define double long double
 
 
 #define vi vector<int>
+#define pii pair<int,int>
 #define sz(x) (int)x.size()
 #define each(x) for(auto &it:x)
 #define all(x) (x).begin(),(x).end()
 #define rall(x) (x).rbegin(),(x).rend()
 #define fastIO cin.tie(0)->sync_with_stdio(0)
 
-
-int T=1;
 
 
 auto solve = []() {
