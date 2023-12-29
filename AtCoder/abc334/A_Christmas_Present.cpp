be8849ee8c025a767b506b342319212e804af942
nxtsourav7 <nxtsourav7@gmail.com>
2023-12-29 10:30:27 +0600

    atcoder

---
+++ AtCoder/abc334/A_Christmas_Present.cpp
@@ -0,0 +1,44 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2023-12-29 10:04:41
+**/
+
+#include<bits/stdc++.h>
+long long int T = 1;
+using namespace std;
+
+
+#define endl "\n"
+#define f first 
+#define s second
+#define int long long 
+#define double long double
+
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define sz(x) (int)x.size()
+#define each(x) for(auto &it:x)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+
+
+auto solve = []() {
+	int b,g;
+    cin >> b >> g;
+
+    cout << ((b>g) ? "Bat":"Glove");
+};
+
+
+int32_t main() {
+	fastIO;
+	// cin >> T;
+	for(int t=1; t<=T; ++t) {
+		// cout << "Case #" << t << ": ";
+		solve();
+        cout << endl;
+	}
+	return 0;
+}
