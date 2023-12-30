6fcfa475194626a0bd6d68b8531d21a61a539d2c
nxtsourav7 <nxtsourav7@gmail.com>
2023-12-30 13:21:09 +0600

    div-3

---
+++ Codeforces/1800/D_Remove_Two_Letters.cpp
@@ -0,0 +1,50 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2023-12-30 11:00:14
+**/
+
+#include<bits/stdc++.h>
+long long int T = 1;
+using namespace std;
+
+
+#define endl "\n"
+#define f first 
+#define s second
+#define int long long 
+#define double long double
+
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define sz(x) (int)x.size()
+#define each(x) for(auto &it:x)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+
+
+auto solve = []() {
+	int n;
+    string s;
+    cin >> n >> s;
+
+    int ans = n-1;
+    for(int i=1; i+1<n; ++i) {
+        if(s[i-1]==s[i+1]) ans--;
+    }
+
+    cout << ans;
+};
+
+
+int32_t main() {
+	fastIO;
+	cin >> T;
+	for(int t=1; t<=T; ++t) {
+		// cout << "Case #" << t << ": ";
+		solve();
+        cout << endl;
+	}
+	return 0;
+}
