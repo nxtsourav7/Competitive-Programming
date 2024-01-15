2b39004b29858a2f01af0b4749dd37fef03a6c69
nxtsourav7 <nxtsourav7@gmail.com>
2024-01-15 19:51:17 +0600

    abc336

---
+++ AtCoder/abc336/A_Long_Loong.cpp
@@ -0,0 +1,55 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-01-14 18:02:39
+**/
+
+#include<bits/stdc++.h>
+long long int T = 1;
+using namespace std;
+
+
+#define endl '\n'
+#define ff first 
+#define ss second
+#define int long long 
+#define double long double
+
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pii>
+#define sz(x) (int)x.size()
+#define each(x) for(auto &it:x)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+
+
+#define read(n) int n;cin >> n;
+#define inVec(x,n) vi x(n);each(x) cin >> it;
+
+
+auto solve = []() {
+	int n;
+    cin >> n;
+
+    string ans = "L";
+    for(int i=1; i<=n; ++i) {
+        ans += 'o';
+    }
+    ans += "ng";
+
+    cout << ans;
+};
+
+
+int32_t main() {
+	fastIO;
+	// cin >> T;
+	for(int t=1; t<=T; ++t) {
+		// cout << "Case #" << t << ": ";
+		solve();
+        cout << endl;
+	}
+	return 0;
+}
