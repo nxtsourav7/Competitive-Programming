10a76289159ed802fa30a7f7a608a52165e07bf3
nxtsourav7 <nxtsourav7@gmail.com>
2024-06-12 22:20:43 +0600

    138

---
+++ CodeChef/START-138/Heat_Wave.cpp
@@ -0,0 +1,34 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-06-12 20:31:15
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long 
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    auto nxtsourav7 = []()->void {
+        int x, y;
+        cin >> x >> y;
+        if(y > x) cout << "YES";
+        else cout << "NO";
+    };
+
+    bool Q = false;
+    int T = Q? (cin>>T, T) : 1;
+    for(int test=1; test<=T; ++test) {
+        // cout << "Case #" << test << ": ";
+        nxtsourav7();
+        cout << endl;
+    }
+    
+    return 0;
+}
