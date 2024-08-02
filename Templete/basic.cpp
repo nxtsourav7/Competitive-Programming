69dffab7609c1c8971cc86f7cde7fb167893927b
nxtsourav7 <nxtsourav7@gmail.com>
2024-08-02 15:55:06 +0600

    update basic

--- Templete/basic.cpp
+++ Templete/basic.cpp
@@ -4,38 +4,25 @@
 **/
 
 #include<bits/stdc++.h>
-long long int T = 1;
 using namespace std;
 
-
-#define nl "\n"
-#define f first 
-#define s second
+#define endl "\n"
 #define int long long 
-#define double long double
-
-
-#define vi vector<int>
-#define pii pair<int,int>
-#define sz(x) (int)x.size()
-#define each(x) for(auto &it:x)
-#define all(x) (x).begin(),(x).end()
-#define rall(x) (x).rbegin(),(x).rend()
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define test(T) for(int t = 1; t <= T; ++t)
 #define fastIO cin.tie(0)->sync_with_stdio(0)
 
-
-auto solve = []() {
-	
-};
-
+void solve() {}
 
 int32_t main() {
-	fastIO;
-	cin >> T;
-	for(int t=1; t<=T; ++t) {
-		// cout << "Case #" << t << ": ";
-		solve();
-        cout << endl;
-	}
-	return 0;
+    fastIO;
+    bool Q = true;
+    int T = Q? (cin >> T, T) : 1;
+    test(T) {
+        solve();
+    }
+    
+    return 0;
 }
