e886b7df5dff9d2ff77e49d5150a5c49b46d3220
nxtsourav7 <nxtsourav7@gmail.com>
2024-12-16 19:19:00 +0600

    9min but ....

---
+++ Codeforces/Educational/1837/B_Comparison_String.cpp
@@ -0,0 +1,45 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-12-16 14:29:21
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long 
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define test(T) for(int t = 1; t <= T; ++t)
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int ans = 1;
+    int cnt = 1;
+    for(int i = 0; i < n - 1; ++i) {
+        if(s[i] == s[i + 1]) cnt++;
+        else cnt = 1;
+
+        ans = max(ans, cnt);
+    }
+
+    cout << ans + 1;
+}
+
+int32_t main() {
+    fastIO;
+    bool Q = true;
+    int T = Q? (cin >> T, T) : 1;
+    test(T) {
+        solve();
+        cout << endl;
+    }
+    
+    return 0;
+}
