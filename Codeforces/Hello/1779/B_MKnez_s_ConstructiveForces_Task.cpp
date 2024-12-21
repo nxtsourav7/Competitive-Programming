a731f1e281aead41ba95d8505b97ad48181ac8cd
nxtsourav7 <nxtsourav7@gmail.com>
2024-12-21 14:01:33 +0600

    add solution for Codeforces Problem B (MKnez's Constructive Forces Task)

---
+++ Codeforces/Hello/1779/B_MKnez_s_ConstructiveForces_Task.cpp
@@ -0,0 +1,57 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-12-20 09:44:49
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long 
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define test(T) for(int t = 1; t <= T; ++t)
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+
+void solve() {
+    int n; cin >> n;
+
+    if(n == 1) {
+        cout << "YES" << endl << 1;
+        return;
+    }
+    if(n == 3) {
+        cout << "NO";
+        return;
+    }
+
+    if(n & 1) {
+        cout << "YES" << endl;
+        int a = n / 2, b = a - 1;
+        for(int i = 0; i < n; ++i) {
+            if(i & 1) cout << -a;
+            else cout << b;
+            cout << " ";
+        }
+    }
+    else {
+        cout << "YES" << endl;
+        for(int i = 1; i <= n; ++i) {
+            if(i & 1) cout << 1 << " ";
+            else cout << -1 << " ";
+        }
+    }
+}
+
+int32_t main() {
+    fastIO;
+    bool Q = true;
+    int T = Q? (cin >> T, T) : 1;
+    test(T) {
+        solve();
+        cout << endl;
+    }
+    
+    return 0;
+}
