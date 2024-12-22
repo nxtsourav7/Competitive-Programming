8b0be0f843e1a73da0d2eb5d61070ccf19d761d3
nxtsourav7 <nxtsourav7@gmail.com>
2024-12-22 13:29:02 +0600

    add solution for Codeforces Problem B (Educational 1749)

---
+++ Codeforces/Educational/1749/B_Death_s_Blessing.cpp
@@ -0,0 +1,43 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-12-22 01:04:07
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long 
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define test(T) for(int t = 1; t <= T; ++t)
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+
+void solve() {
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N);
+    int ans = 0;
+    for(auto &x : A) {
+        cin >> x;
+        ans += x;
+    }
+    for(auto &x : B) {
+        cin >> x; 
+        ans += x;   
+    }
+    cout << ans - *max_element(all(B));
+}
+
+int32_t main() {
+    fastIO;
+    bool Q = true;
+    int T = Q? (cin >> T, T) : 1;
+    test(T) {
+        solve();
+        cout << endl;
+    }
+    
+    return 0;
+}
