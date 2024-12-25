820a4817ea76e1f8eac8ac078410ca688e380656
nxtsourav7 <nxtsourav7@gmail.com>
2024-12-25 23:51:20 +0600

    hard one is binray search .

---
+++ Codeforces/Div-2/2005/B_2_The_Strict_Teacher_Hard_Version.cpp
@@ -0,0 +1,47 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-12-25 13:12:04
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long 
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define test(T) for(int t = 1; t <= T; ++t)
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+
+void solve() {
+    int n, m,q;
+    cin >> n >> m >> q;
+    vector<int> A(m);
+    for(auto &x : A) cin >> x;
+    sort(all(A));
+    while(q--) {
+        int x;
+        cin >> x;
+
+        int idx = lower_bound(all(A), x) - A.begin();
+        // cerr << idx << endl;
+        if(idx == 0) cout << A[0] - 1 << endl;
+        else if(idx == m) cout << n - A[m-1] << endl;
+        else {
+            cout << (A[idx] - A[idx - 1]) / 2 << endl;
+        }
+    }
+}
+
+int32_t main() {
+    fastIO;
+    bool Q = true;
+    int T = Q? (cin >> T, T) : 1;
+    test(T) {
+        solve();
+        cout << endl;
+    }
+    
+    return 0;
+}
