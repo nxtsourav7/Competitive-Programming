e07c76c378b215200dae0397f32ac28c0fbc7535
nxtsourav7 <nxtsourav7@gmail.com>
2024-12-28 04:01:32 +0600

    800, 15 min

---
+++ Codeforces/Div-2/1983/A_Array_Divisibility.cpp
@@ -0,0 +1,30 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-12-28 03:57:38
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void S0LVE() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; ++i) {
+        cout << i << ' ';
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Q;
+    cin >> Q;
+    while(Q--) {
+        S0LVE();
+        cout << '\n';
+    }
+
+    return 0;
+}
