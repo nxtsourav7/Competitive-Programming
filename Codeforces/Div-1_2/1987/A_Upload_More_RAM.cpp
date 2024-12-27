bfeb062a1fb4cb83040e3d84f029308f1dc1c58c
nxtsourav7 <nxtsourav7@gmail.com>
2024-12-28 01:25:27 +0600

    3 min , 800

---
+++ Codeforces/Div-1_2/1987/A_Upload_More_RAM.cpp
@@ -0,0 +1,28 @@
+/**
+ *	author 	: nxtsourav7
+ *	created : 2024-12-28 01:23:41
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void S0LVE() {
+    int n, k;
+    cin >> n >> k;
+
+    cout << (n - 1) * k + 1;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Q;
+    cin >> Q;
+    while(Q--) {
+        S0LVE();
+        cout << '\n';
+    }
+
+    return 0;
+}
